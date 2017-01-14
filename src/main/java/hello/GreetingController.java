b0dc8ab0edce2442e337adc488c9baffb9f67197
Xuefeng Wu <benewu@gmail.com>
2017-01-14 11:48:05 +0800

    clear cache when datasource changed

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Autowired
     private CustomerRepository customers;
@@ -19,6 +24,7 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public Customer greeting(@RequestParam(value="name", defaultValue="SILVER") String name) {
         CustomerContextHolder.setCustomerType(CustomerContextHolder.CustomerType.valueOf(name.toUpperCase()));
+        entityManager.clear();
         return customers.findOne(1L);
         //return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
